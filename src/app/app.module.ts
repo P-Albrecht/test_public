7e74a7f44f5cc8eb6a6e91a85c203bddc7246a14
P-Albrecht <83654540+P-Albrecht@users.noreply.github.com>
2024-02-08 14:23:53 +0000

    s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { FlaskapiService } from "./flaskapi.service";
 import { EnviromentComponent } from './enviroment/enviroment.component';
+import { EnviromentModule } from './enviroment/enviroment.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EnviromentComponent,
+    //EnviromentComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
+    EnviromentModule
   ],
   providers: [FlaskapiService],
   bootstrap: [AppComponent]
---
+++ src/app/enviroment/enviroment.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { EnviromentComponent } from './enviroment.component';
+
+@NgModule({
+  declarations: [
+    EnviromentComponent,
+  ],
+  imports: [],
+  exports: [EnviromentComponent],
+  providers: [],
+  bootstrap: []
+})
+export class EnviromentModule { }
