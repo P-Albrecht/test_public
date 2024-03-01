ac5a134b4fc323737ccd6dbeeaa655489d73ef07
P-Albrecht <83654540+P-Albrecht@users.noreply.github.com>
2024-03-01 12:23:39 +0100

    Update enviroment.module.ts

--- src/app/enviroment/enviroment.module.ts
+++ src/app/enviroment/enviroment.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { EnviromentComponent } from './enviroment.component';
 import { CommonModule } from '@angular/common';
 
-
 @NgModule({
   declarations: [
     EnviromentComponent,
   ],
-  imports: [CommonModule],
-  exports: [EnviromentComponent],
-  providers: [],
-  bootstrap: []
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    EnviromentComponent,
+  ],
+  providers: [
+  ],
+  bootstrap: [
+  ]
 })
 export class EnviromentModule { }
