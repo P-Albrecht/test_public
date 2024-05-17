9e90fdcf3255295511411a3d5c74b51e04d1a137
P-Albrecht <83654540+P-Albrecht@users.noreply.github.com>
2024-05-17 14:25:22 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {ActivatedRoute, Params, Router} from "@angular/router";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 import { HttpClient } from '@angular/common/http';
 import { FlaskapiService } from './flaskapi.service';
 
