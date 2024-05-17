22280b648527c7550241c2ebbc47b097b7eec05b
P-Albrecht <83654540+P-Albrecht@users.noreply.github.com>
2024-05-17 14:11:54 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 import {ActivatedRoute, Params, Router} from "@angular/router";
 import { HttpClient } from '@angular/common/http';
-
 import { FlaskapiService } from './flaskapi.service';
-
 
 @Component({
   selector: 'app-root',
